e70cd46789fbe1fa783fba18d6f8a794bd7cc465
Dark_Trigger <withanageyasiru@gmail.com>
2019-04-14 15:45:36 +0530

    complete V1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.577523a6e14a29b92ee145b8da2149de.js"
+  "/Company_Assist/precache-manifest.734822648c4c8b04b5af8a9a857f8e60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Company_Assist/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
